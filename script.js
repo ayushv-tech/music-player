121fcc36b6fd377d6c07591831d34fcaf6301e9b
Ayush Verma <ayush.verma0106@gmail.com>
2026-06-06 15:53:25 +0530

    Updated music player UI and features

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
 let audio = document.getElementById("audio");
 
-let playBtn = document.getElementById("play");
-let pauseBtn = document.getElementById("pause");
+let playPauseBtn = document.getElementById("playPause");
 let progress = document.getElementById("progress");
 let volume = document.getElementById("volume");
+audio.volume = 0.5;
+volume.value = 0.5;
 
-playBtn.addEventListener("click", function(){
-    audio.play();
+volume.addEventListener("input",function() {
+    audio.volume = volume.value;
 });
 
-pauseBtn.addEventListener("click", function(){
-    audio.pause();
+playPauseBtn.addEventListener("click", function(){
+
+    if(audio.paused){
+        audio.play();
+        playPauseBtn.innerHTML =  '<i class="fa-solid fa-play"></i>';
+    }
+    else{
+        audio.pause();
+        playPauseBtn.innerHTML =  '<i class="fa-solid fa-pause"></i>';
+    }
+
 });
+
 let nextBtn = document.getElementById("next");
 let prevBtn = document.getElementById("prev");
 
 let volumeIcon = document.getElementById("volume-icon");
+
+volumeIcon.addEventListener("click", function() {
+    if(audio.volume > 0){
+        audio.volume = 0;
+        volume.value = 0;
+        volumeIcon.className = "fa-solid fa-volume-xmark";
+    } else {
+        audio.volume = 0.5;
+        volume.value = 0.5;
+        volumeIcon.className = "fa-solid fa-volume-high";
+    }
+});
+
 
 nextBtn.addEventListener("click", function(){
     alert("Next Song");
@@ -52,3 +76,47 @@ volume.addEventListener("input", function(){
     }
 
 });
+
+let time = document.getElementById("time");
+
+audio.addEventListener("loadedmetadata", function () {
+    let durationMin = Math.floor(audio.duration / 60);
+    let durationSec = Math.floor(audio.duration % 60);
+
+    if (durationSec < 10) durationSec = "0" + durationSec;
+
+    time.innerText = `00:00 / ${durationMin}:${durationSec}`;
+});
+
+audio.addEventListener("timeupdate", function () {
+    let currentMin = Math.floor(audio.currentTime / 60);
+    let currentSec = Math.floor(audio.currentTime % 60);
+
+    if (currentSec < 10) currentSec = "0" + currentSec;
+
+    let durationMin = Math.floor(audio.duration / 60);
+    let durationSec = Math.floor(audio.duration % 60);
+
+    if (durationSec < 10) durationSec = "0" + durationSec;
+
+    time.innerText =
+        `${currentMin}:${currentSec} / ${durationMin}:${durationSec}`;
+});
+
+if(audio.paused){
+    audio.play();
+    playPauseBtn.innerHTML =  '<i class="fa-solid fa-play"></i>';
+}
+else{
+    audio.pause();
+    playPauseBtn.innerHTML =  '<i class="fa-solid fa-pause"></i>';
+}
+
+if(audio.paused){
+    audio.play();
+    playPauseBtn.innerHTML = '<i class="fa-solid fa-pause"></i>';
+}
+else{
+    audio.pause();
+    playPauseBtn.innerHTML = '<i class="fa-solid fa-play"></i>';
+}
